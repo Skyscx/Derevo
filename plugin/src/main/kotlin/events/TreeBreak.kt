f040633a162763ce98748affc65b0a2215763da9
Skyscx <danakis20@gmail.com>
2024-06-07 15:36:03 +0300

    Trash derevorubka

--- plugin/src/main/kotlin/events/TreeBreak.kt
+++ plugin/src/main/kotlin/events/TreeBreak.kt
@@ -2,98 +2,46 @@ package events
 
 import App
 import logic.Logic
-import org.bukkit.Material
-import org.bukkit.block.Block
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
-import org.bukkit.scheduler.BukkitRunnable
 
-class TreeBreak(private val plugin: App) : Listener {
+class TreeBreak(plugin: App) : Listener {
     private val logic = Logic(plugin)
-    private val treeList = logic.getTreeMaterials()
     @EventHandler
     fun onBlockBreak(event: BlockBreakEvent){
         val tool = event.player.inventory.itemInMainHand.type
         val player = event.player
-        val block = event.block
-        val blockType = block.type
         if (logic.checkTypeToolsBreak(tool)){
-            //val blockBreak = event.block
-            //val blockBreakType = event.block.type
-            if (blockType !in treeList){ return } //Проверка на тип блока
+            event.isCancelled = true
+            val block = event.block
+            val blockType = block.type
+            val tree = logic.getTreeMaterials()
+            if (blockType !in tree){ return }
+            val listBlocks = logic.findTreeBlocks(block)
+            logic.processingTreeBlocks(listBlocks, player)
+            /*
+            if (logic.checkTypeTreeBlock(blockType)){
+                logic.replacerTreeToFence(blockType, block)
+                logic.giveTreeToPlayer(player, blockType)
+                logic.replacerFenceToTree(block, blockType, 20*10)
 
-            // Поиск всех блоков дерева, связанных со сломанным блоком
-            val treeBlocks = mutableListOf<Block>()
-            logic.getTree(block, treeBlocks)
-
-            for (currentBlock in treeBlocks){
-                currentBlock.type = Material.AIR
-            }
-            println(blockType)
-            val fenceType = logic.replacerTreeToFence(blockType)
-            println(fenceType)
-            if (fenceType == null) {
-                player.sendMessage("Не удалось определить тип забора для дерева")
-
-                return
             }
 
-            val queue = ArrayDeque<Block>(treeBlocks)
-
-            val timer = object : BukkitRunnable(){
-                override fun run(){
-                    //queue empty
-                    if (queue.isEmpty()){
-                        cancel()
-                        return
-                    }
-
-                    //getBlockInQueue
-                    val currentBlock = queue.removeFirst()
-                    currentBlock.type = fenceType
-                    logic.giveTreeToPlayer(player, currentBlock.type)
-
-                    if (currentBlock.type in treeList){
-                        val trunkBlocks = mutableListOf<Block>()
-                        queue.addAll(trunkBlocks)
-                    }
-                }
-            }
-
-            timer.runTaskTimer(plugin, 1L, 1L)
-
-            /**
-             * for ((index, currentBlock) in treeBlocks.withIndex()) {
-             *                 logic.replacerFenceToTree(currentBlock, fenceType, index * 2)
-             *                 player.inventory.addItem(ItemStack(blockType, 1))
-             *
-             *             }
-             *
-             *
-             * **/
-            event.isCancelled
-
-            /**
-
-            if (logic.checkTypeTreeBlock(blockBreakType)){
-                logic.replacerTreeToFence(blockBreakType, blockBreak)
-                logic.giveTreeToPlayer(player, blockBreakType)
-                logic.replacerFenceToTree(blockBreak, blockBreakType, 20*10) // 10 sec regeneration
-                event.isCancelled = true
-            }
-            if (logic.checkTypeFenceBlock(blockBreakType)){
-                val blockReplace = logic.giveFoundBlock(blockBreak)
-                if (blockReplace != null){
+            if(logic.checkTypeFenceBlock(blockType)) {
+                val blockReplace = logic.giveFoundBlock(block)
+                if (blockReplace != null) {
                     val blockReplaceType = blockReplace.type
-                    if (logic.replacerTreeToFenceInRadius(blockReplace)){
+                    if (logic.replacerTreeToFenceInRadius(blockReplace)) {
                         logic.giveTreeToPlayer(player, blockReplaceType)
-                        logic.replacerFenceToTree(blockReplace, blockReplaceType, 20*10) // 10 sec regeneration
+                        logic.replacerFenceToTree(blockReplace, blockReplaceType, 20*10)
                     }
                 }
                 event.isCancelled = true
+
             }
-            **/
+            */
+
         }
     }
 }
--- plugin/src/main/kotlin/logic/Logic.kt
+++ plugin/src/main/kotlin/logic/Logic.kt
@@ -6,13 +6,14 @@ import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitRunnable
 import java.util.*
-import kotlin.experimental.and
 
 
 class Logic(private val plugin: App) {
-    fun replacerTreeToFence(tree: Material): Material? {
-        return when (tree) {
+    //Замена материала установленного блока с древесины на забор
+    private fun replacerTreeToFence(tree: Material, block: Block){
+        val fenceType = when (tree) {
             Material.OAK_LOG -> Material.OAK_FENCE
             Material.BIRCH_LOG -> Material.BIRCH_FENCE
             Material.SPRUCE_LOG -> Material.SPRUCE_FENCE
@@ -21,11 +22,13 @@ class Logic(private val plugin: App) {
             Material.JUNGLE_LOG -> Material.JUNGLE_FENCE
             else -> null
         }
+        if (fenceType != null) block.type = fenceType
     }
+    //Получение случайного найденного блока
     fun giveFoundBlock(centerBlock: Block): Block? {
         return getRandomTreeBlock(centerBlock)
     }
-    fun replacerTreeToFenceInRadius(foundBlock: Block) : Boolean{
+    /*fun replacerTreeToFenceInRadius(foundBlock: Block) : Boolean{
         val fenceType = when (foundBlock.type) {
             Material.OAK_LOG -> Material.OAK_FENCE
             Material.BIRCH_LOG -> Material.BIRCH_FENCE
@@ -40,27 +43,77 @@ class Logic(private val plugin: App) {
             return true
         }
     return false
-    }
-    fun replacerFenceToTree(block: Block, newMaterial: Material, ticks: Int) {
+    }*/
+    //Замена материала установленного блока с забором на древесину с задержкой
+    private fun replacerFenceToTree(block: Block, newMaterial: Material, ticks: Int) {
         Bukkit.getScheduler().runTaskLater(plugin, Runnable {
             block.type = newMaterial
         }, ticks.toLong())
     }
+    //Проверка на содержание материала в списке с деревьями. Возращение Boolean
     fun checkTypeTreeBlock(tree: Material) : Boolean {
         val treeMaterials = getTreeMaterials()
         return treeMaterials.contains(tree)
     }
+    //Проверка на содержание материала в списке с забором. Возращение Boolean
     fun checkTypeFenceBlock(fence: Material) : Boolean{
         val fenceMaterial = getFenceMaterials()
         return fenceMaterial.contains(fence)
     }
+    //Проверка, каким предметом был сломан блок. Возращение Boolean
     fun checkTypeToolsBreak(tools: Material) : Boolean{
         val toolsMaterial = getToolsList()
         return toolsMaterial.contains(tools)
     }
-    fun giveTreeToPlayer(player: Player, tree: Material){
+    //Выдача блока древесины игроку
+    private fun giveTreeToPlayer(player: Player, tree: Material){
         player.inventory.addItem(ItemStack(tree))
     }
+    //Поиск блоков древесины
+    fun findTreeBlocks(centerBlock: Block): List<Block>{
+        val listBlocks = mutableListOf<Block>()
+        val listMaterial = getTreeMaterials()
+        if (centerBlock.type !in listMaterial){
+            println("Сломанный блок не содержит данных древесины!")
+            return emptyList()
+        }
+        // Создайте очередь для хранения блоков, которые должны быть обработаны
+        val queue = ArrayDeque<Block>()
+        queue.add(centerBlock)
+        // Обработайте блоки в очереди
+        while (queue.isNotEmpty()) {
+            val currentBlock = queue.removeFirst()
+            // Если блок не в списке, добавьте его и найдите его соседей
+            if (currentBlock !in listBlocks) {
+                listBlocks.add(currentBlock)
+                val neighbors = findTreeInCube(currentBlock)
+                // Добавьте соседей в очередь, если они не в списке
+                for (neighbor in neighbors) {
+                    if (neighbor !in listBlocks) {
+                        queue.add(neighbor)
+                    }
+                }
+            }
+        }
+        println("Size blocks tree: " + listBlocks.size)
+        return listBlocks
+    }
+    //Обработка списка с найденными блоками
+    fun processingTreeBlocks(treeBlocks: List<Block>, player: Player){
+        val sortedTreeBlocks = treeBlocks.sortedBy { block -> block.location.y }
+
+        object : BukkitRunnable(){
+            override fun run() {
+                for (block in sortedTreeBlocks) {
+                    val blockType = block.type
+                    replacerTreeToFence(blockType, block)
+                    giveTreeToPlayer(player, blockType)
+                    Thread.sleep(500L)
+                }
+            }
+        }.runTaskAsynchronously(plugin)
+    }
+    //Приватный метод который находит все однотипные блоки в радиусе 3x3x3
     private fun findTreeInCube(centerBlock: Block): List<Block> {
         val foundBlocks = mutableListOf<Block>()
         val targetMaterials = getTreeMaterials()
@@ -69,7 +122,7 @@ class Logic(private val plugin: App) {
                 for (z in -1..1) {
                     val block = centerBlock.getRelative(x, y, z)
                     if (targetMaterials.contains(block.type) && block.type != Material.AIR) {
-                        val typeLog = convertFenceToLog(centerBlock)
+                        val typeLog = centerBlock.type
                         if (block.type == typeLog) {
                             foundBlocks.add(block)
                         }
@@ -79,6 +132,7 @@ class Logic(private val plugin: App) {
         }
         return foundBlocks
     }
+    //Конвертация материала с забора на древесину
     private fun convertFenceToLog(block: Block): Material? {
         val fenceType = block.type
         var logType: Material? = null
@@ -93,6 +147,7 @@ class Logic(private val plugin: App) {
         }
         return logType
     }
+    //Получение случайного блока из области 3x3x3
     private fun getRandomTreeBlock(centerBlock: Block): Block? {
         val foundBlocks = findTreeInCube(centerBlock)
         if (foundBlocks.isEmpty()) { return null }
@@ -100,6 +155,7 @@ class Logic(private val plugin: App) {
         val index = random.nextInt(foundBlocks.size)
         return foundBlocks[index]
     }
+    //Список древесины
     fun getTreeMaterials(): List<Material> {
         val treeMaterials = mutableListOf<Material>()
         treeMaterials.add(Material.OAK_LOG)
@@ -110,6 +166,7 @@ class Logic(private val plugin: App) {
         treeMaterials.add(Material.DARK_OAK_LOG)
         return treeMaterials
     }
+    //Список заборов
     private fun getFenceMaterials(): List<Material> {
         val fenceMaterials = mutableListOf<Material>()
         fenceMaterials.add(Material.OAK_FENCE)
@@ -120,6 +177,7 @@ class Logic(private val plugin: App) {
         fenceMaterials.add(Material.DARK_OAK_FENCE)
         return fenceMaterials
     }
+    //Список инструментов
     private fun getToolsList(): List<Material>{
         val toolsMaterials = mutableListOf<Material>()
         toolsMaterials.add(Material.GOLDEN_AXE)
@@ -127,24 +185,4 @@ class Logic(private val plugin: App) {
         return toolsMaterials
     }
 
-    fun getTree(block: Block, blocks: MutableList<Block>) {
-        val data = block.blockData as Byte
-        val width = if ((data and 0x10.toByte()) != 0.toByte()) 2 else 1
-        val height = if ((data.toInt() and 0x0F) == 1) 3 else 2
-
-        for (i in 0 until width) {
-            for (j in 0 until height) {
-                for (k in 0 until width) {
-                    val x = block.x + i - (width - 1) / 2
-                    val y = block.y + j
-                    val z = block.z + k - (width - 1) / 2
-                    val currentBlock = block.world.getBlockAt(x, y, z)
-
-                    if (currentBlock.type == block.type) {
-                        blocks.add(currentBlock)
-                    }
-                }
-            }
-        }
-    }
 }
